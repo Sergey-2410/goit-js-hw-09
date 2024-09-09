3e30d1bcdb294f6d28264906deadaf9599ba14f5
Sergey-2410 <167428697+Sergey-2410@users.noreply.github.com>
2024-09-09 11:15:42 +0000

    Deploying to gh-pages from @ Sergey-2410/goit-js-hw-09@d9c73fcb14640e327bc057b35dfcf693280fa6df 🚀

---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/styles-BugZaheq.js";let e={email:"",message:""};const a=document.querySelector(".feedback-form"),l="feedback-form-state",r=localStorage.getItem(l);r&&(e=JSON.parse(r),a.email.value=e.email,a.message.value=e.message);a.addEventListener("input",t=>{e[t.target.name]=t.target.value.trim(),localStorage.setItem(l,JSON.stringify(e))});a.addEventListener("submit",t=>{if(t.preventDefault(),!e.email||!e.message){alert("Fill please all fields");return}console.log(e),localStorage.removeItem(l),a.reset()});
+//# sourceMappingURL=2-form.js.map
